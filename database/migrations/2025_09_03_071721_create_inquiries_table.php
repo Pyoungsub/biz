869f00d93bfb0f76145a860479cd4c7b7cea9c2f
Pyoungsub <ps.shim87@gmail.com>
2025-09-04 23:54:36 +0900

    contact

--- database/migrations/2025_09_03_071721_create_inquiries_table.php
+++ database/migrations/2025_09_03_071721_create_inquiries_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('inquiries', function (Blueprint $table) {
             $table->id();
             $table->foreignId('inquiry_type_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');;
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('message');
             $table->timestamps();
         });
